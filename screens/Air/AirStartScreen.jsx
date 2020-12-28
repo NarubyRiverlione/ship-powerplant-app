6edb08055a170c3e7f13fa4aa1f5fc48bf8be766
Naruby Riverlione <naruby.riverlione@gmail.com>
2020-12-28 12:04:47 +0100

    start air svg WIP

--- screens/Air/AirStartScreen.jsx
+++ screens/Air/AirStartScreen.jsx
@@ -1,5 +1,7 @@
-import * as React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react'
+import { View } from 'react-native'
+import { observer } from 'mobx-react-lite'
+import Svg, { Text } from 'react-native-svg'
 
 import SimContext from '../../SimulatorContext'
 import SimulatorScreen from '../SimulatorScreen'
@@ -7,7 +9,7 @@ import TankValves from '../../components/svg/TankValves'
 import Valve from '../../components/svg/Valve'
 import Pipe from '../../components/svg/Pipe'
 
-const AirStartScreen = () => {
+const AirStartScreen = observer(() => {
   const Sim = SimContext()
   const {
     AirSys: {
@@ -17,20 +19,30 @@ const AirStartScreen = () => {
 
   return (
     <SimulatorScreen>
-      <View style={styles.container}>
-        <View style={{
-          flex: 1, flexDirection: 'column', margin: 20, justifyContent: 'flex-start',
-        }}
-        />
-        <TankValves
-          X={200}
-          Y={10}
-          Name="Emergency receiver"
-          TankSys={EmergencyReceiver}
-        />
+      <View style={{ flex: 1, flexDirection: 'column' }}>
+        <Svg width="100%" height="100%">
+
+          <Pipe x1={250} y1={400} x2={300} y2={400} ContentColor="grey" HasContent={EmergencyCompressor.Content() !== 0} />
+          <Valve
+            X={300}
+            Y={400}
+            LeftToRight
+            Position={EmergencyOutletValve.isOpen}
+            cb={() => EmergencyOutletValve.Toggle()}
+          />
+          <Pipe x1={380} y1={400} x2={660} y2={400} ContentColor="grey" HasContent={EmergencyOutletValve.Content() !== 0} />
+
+          <TankValves
+            X={650}
+            Y={350}
+            Name="Emergency receiver"
+            TankSys={EmergencyReceiver}
+            ContentColor="grey"
+          />
+        </Svg>
       </View>
     </SimulatorScreen>
   )
-}
+})
 
 export default AirStartScreen
