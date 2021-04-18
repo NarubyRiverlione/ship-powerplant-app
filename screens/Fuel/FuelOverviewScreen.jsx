d16bb1b82571f508643fad895a04a1b8d8af645c
Naruby Riverlione <naruby.riverlione@gmail.com>
2021-04-18 17:32:53 +0200

    Expo SDK 41

--- screens/Fuel/FuelOverviewScreen.jsx
+++ screens/Fuel/FuelOverviewScreen.jsx
@@ -14,18 +14,13 @@ const FuelOverviewScreen = observer(({ navigation }) => {
   const {
     DsFuelSys: { DsStorage, DsService },
     HfFuelSys: {
-      HfForeBunker, HfAftBunker, HfPortBunker, HfStarboardBunker,
+      HfForeBunker, HfAftBunker, HfPortBunker, HfStarboardBunker, HfSettelingTank, HfServiceTank,
     },
   } = Sim
   return (
     <SimulatorScreen>
       <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-around' }}>
-        {/*
-  <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'flex-start' }}>
-  <Text style={styles.subTitle}>Storage</Text>
-  <Button style={{ flex: 1 }} title="Go to storage" onPress={() => navigation.navigate('FuelStorageScreen')} />
-  </View>
-*/}
+
         <View style={{ flex: 1 }}>
           <Text style={styles.title}>Diesel oil storage</Text>
           <View style={{
@@ -40,30 +35,46 @@ const FuelOverviewScreen = observer(({ navigation }) => {
           </View>
         </View>
 
-        <View style={{ flex: 2, justifyContent: 'center' }}>
+        <View style={{ flex: 2 }}>
           <Text style={styles.title}>Heavy fuel storage</Text>
           <View style={{
             flex: 1, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center',
           }}
           >
-            <Text style={styles.text}>{`Fore bunker tank ${HfForeBunker.Content / HfForeBunker.Tank.Volume * 100} % `}</Text>
-            <Text style={styles.text}>{`Port bunker tank ${HfPortBunker.Content / HfPortBunker.Tank.Volume * 100} % `}</Text>
+            <Text style={styles.text}>{`Fore bunker tank ${(HfForeBunker.Content / HfForeBunker.Tank.Volume * 100).toFixed(0)} % `}</Text>
+            <Text style={styles.text}>{`Port bunker tank ${(HfPortBunker.Content / HfPortBunker.Tank.Volume * 100).toFixed(0)} % `}</Text>
             <Text style={styles.text}>{`Starboard bunker tank ${HfStarboardBunker.Content / HfStarboardBunker.Tank.Volume * 100} % `}</Text>
             <Text style={styles.text}>{`Aft bulk tank ${HfAftBunker.Content / HfAftBunker.Tank.Volume * 100} % `}</Text>
+          </View>
+          <View style={{
+            flex: 1, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center',
+          }}
+          >
+            <Text style={styles.text}>{`Fore bunker tank ${HfForeBunker.Temperature}°C `}</Text>
+            <Text style={styles.text}>{`Port bunker tank ${HfPortBunker.Temperature}°C  `}</Text>
+            <Text style={styles.text}>{`Starboard bunker tank ${HfStarboardBunker.Temperature}°C `}</Text>
+            <Text style={styles.text}>{`Aft bulk tank ${HfAftBunker.Temperature}°C  `}</Text>
           </View>
           <View style={{ flex: 1, justifyContent: 'flex-start' }}>
             <Button title="Go to heavy fuel storage" onPress={() => navigation.navigate(NavScreen.Fuel.HfStorageScreen)} />
           </View>
         </View>
 
-        <View style={{ flex: 1 }}>
+        <View style={{ flex: 2 }}>
           <Text style={styles.title}>Heavy fuel service</Text>
           <View style={{
             flex: 1, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center',
           }}
           >
-            <Text style={styles.text}>{`Heavy fuel setteling tank ${0} % full`}</Text>
-            <Text style={styles.text}>{`Heavy fuel service tank ${0} % full`}</Text>
+            <Text style={styles.text}>{`Setteling tank ${(HfSettelingTank.Content / HfSettelingTank.Tank.Volume * 100).toFixed(0)} % full`}</Text>
+            <Text style={styles.text}>{`Service tank ${(HfServiceTank.Content / HfServiceTank.Tank.Volume * 100).toFixed(0)} % full`}</Text>
+          </View>
+          <View style={{
+            flex: 1, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center',
+          }}
+          >
+            <Text style={styles.text}>{`Setting  tank ${HfSettelingTank.Temperature}°C `}</Text>
+            <Text style={styles.text}>{`Service tank ${HfServiceTank.Temperature}°C `}</Text>
           </View>
           <View style={{ flex: 1, justifyContent: 'flex-start' }}>
             <Button title="Go to heavy fuel service" onPress={() => navigation.navigate(NavScreen.Fuel.HfServiceScreen)} />
